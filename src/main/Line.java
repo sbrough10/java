35fe32f8f8bdfbca7574fa7caae368cbe6336914
Broughton, Stephen <Stephen.Broughton@inin.com>
2015-05-28 12:26:24 -0400

    no message

--- src/main/Line.java
+++ src/main/Line.java
@@ -5,11 +5,17 @@ package main;
  */
 public class Line extends Path {
 
-    public Line(Vector vec0, Vector vec1){
-        super(vec0, vec1);
+    public Line(Vector nodeA, Vector nodeB){
+        super(nodeA, nodeB);
     }
 
     public Line(float x0, float y0, float x1, float y1){
         this(new BasicVector(x0, y0), new BasicVector(x1, y1));
     }
+
+    public Vector getDirection(){
+        return new BasicVector(nodeB.getX() - nodeA.getX(), nodeB.getY() - nodeA.getY());
+    }
+
+
 }
--- src/main/Region.java
+++ src/main/Region.java
@@ -5,5 +5,37 @@ package main;
  */
 public class Region {
 
-    LinkListNode<Path> edges = new LinkListNode<>(null);
+    LinkListNode.Chain<Path> edges = new LinkListNode.Chain<>();
+    Vector lastNode;
+
+    public Region(Vector nodeA){
+        lastNode = nodeA;
+    }
+
+    public Region addLine(Vector nodeB){
+        edges.link(new Line(lastNode, nodeB));
+        lastNode = nodeB;
+        return this;
+    }
+
+    public Region addCurve(Vector ctrlA, Vector ctrlB, Vector nodeB){
+        edges.link(new Curve(lastNode, ctrlA, ctrlB, nodeB));
+        lastNode = nodeB;
+        return this;
+    }
+
+    public Region close(){
+        if(!isClosed()){
+            addLine(edges.anteNode.value.nodeA);
+        }
+        return this;
+    }
+
+    public boolean isClosed(){
+        return edges.anteNode.value.nodeA == lastNode;
+    }
+
+    public float getArea(){
+        return 0;
+    }
 }
